e909f051437c749d3f4332f7c0a852960b719e5f
Siddharth <siddharth.sd.dixit@gmail.com>
2023-12-08 16:02:19 +0100

    Added a script (classification_script.py) to loop through all the files in a given folder and allow the user to classify they through the console. Also added functionality in plot_utils.py:plot_image to add a title to the generated plot.

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -3,24 +3,25 @@ from pyraws.utils.visualization_utils import equalize_tensor
 import matplotlib.pyplot as plt
 import torch
 
-# ############################################################# Assignment functions
-def plot_image(image, equalize=True, downsampling_factor=(2,2),figsize=(20,20)):
 
+# ############################################################# Assignment functions
+def plot_image(image, equalize=True, downsampling_factor=(2, 2), figsize=(20, 20), title=""):
     # Applying reshaping and downsampling
-    image=image.permute(2, 1, 0)
+    image = image.permute(2, 1, 0)
     image = image[::downsampling_factor[0], ::downsampling_factor[1]]
     if not equalize:
-        fig, ax = plt.subplots(1,1,figsize=figsize)
-        ax.imshow(image,figsize=figsize)
-        return fig
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+        ax.imshow(image, figsize=figsize)
     else:
-        image=image.type(torch.float32)
-        fig, ax = plt.subplots(1,2,figsize=figsize)
-        ax[0].imshow(image/image.max())
+        image = image.type(torch.float32)
+        fig, ax = plt.subplots(1, 2, figsize=figsize)
+        ax[0].imshow(image / image.max())
         ax[0].set_title("Raw")
         ax[1].imshow(image)
-        image_equalized=equalize_tensor(image)
-        ax[1].imshow(image_equalized/image_equalized.max())
+        image_equalized = equalize_tensor(image)
+        ax[1].imshow(image_equalized / image_equalized.max())
         ax[1].set_title("Filtered")
-        return fig
-
+        if title:
+            fig.suptitle(title)
+    plt.tight_layout()
+    return fig
